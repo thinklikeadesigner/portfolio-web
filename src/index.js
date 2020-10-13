afe14cfb029336123a8a8c552c718f246d834879
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-12 21:11:44 -0500

    asdf

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
 injectTapEventPlugin();
 ReactDOM.render(
-  <App />,
+  <Router basename={`/portfolio-app-copy`}>
+<App />
+</Router>, 
   document.getElementById('root')
 );

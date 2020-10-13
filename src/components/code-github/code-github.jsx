b96ea9ad3e314db745b1d5657a55140bee44daf1
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-12 21:35:42 -0500

    checkout the code on github

--- src/components/code-github/code-github.jsx
+++ src/components/code-github/code-github.jsx
@@ -14,18 +14,18 @@ class CodeGithubComponent extends Component {
     render() {
         return (
             <div className="code-github">
-              <p className="headline">Look at my code, my code is amazing</p>
+              <p className="headline">Check out my other projects!</p>
                 <Card>
                   <CardHeader
                     subtitle="Github Repository"
-                    title="salomonelli/personal-portfolio"
+                    title="thinklikeadesigner/portfolio-web"
                     avatar="github.png"
                   />
                 <CardText>
-                        This application is made with React and Material UI. zCheck out the code on Github.
+                        This application is made with React and Material UI. Check out the code on Github.
                     </CardText>
                     <CardActions>
-                        <FlatButton label="View on github" href="https://github.com/salomonelli/personal-portfolio" target="_blank"/>
+                        <FlatButton label="View on github" href="https://github.com/thinklikeadesigner/portfolio-web" target="_blank"/>
                     </CardActions>
                 </Card>
             </div>
